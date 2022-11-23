6f2e4022069657d4c9813bd3c17704764bebe589
mathanjavaspringbootdev@gmail.com <Admin@DESKTOP-TTUF593>
2022-11-23 08:45:40 +0530

    Example check in for ramadoss

---
+++ FirstExample/src/com/ref/javaclass/example/FindLarge.java
@@ -0,0 +1,30 @@
+package com.ref.javaclass.example;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class FindLarge {
+
+	public FindLarge() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter the number to elements?");
+		int limit = scr.nextInt();
+		int[] array = new int[limit];
+		for(int index = 0; index < limit; index++) {
+			System.out.println("Enter element " + (index + 1));
+			array[index] = scr.nextInt();
+		}
+		int large = array[0];
+		for(int index = 1; index < limit; index++) {
+			if(large < array[index]) {
+				large = array[index];
+			}
+		}
+		System.out.println("Largest Element is>>>>" + large);
+	}
+		
+}
